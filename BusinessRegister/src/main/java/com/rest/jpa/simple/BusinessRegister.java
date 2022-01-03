e350e5094af95dc2c372b7ca75e28712c3007db0
msfata <msfata@me.com>
2022-01-03 19:14:41 +0000

    Rename Main

--- BusinessRegister/src/main/java/com/rest/jpa/simple/BusinessRegister.java
+++ BusinessRegister/src/main/java/com/rest/jpa/simple/BusinessRegister.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailProjectApplication {
+public class BusinessRegister {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailProjectApplication.class, args);
+		SpringApplication.run(BusinessRegister.class, args);
 	}
 }
